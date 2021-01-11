2b38cf5e27d76bab34a156ddc42c1894e7afa368
Amit Daniel Kachhap <amit.kachhap@arm.com>
2021-01-11 18:47:23 +0530

    fs/filesystems: Modify syscall sysfs to accept capability arguments
    Modify the argument parameter from unsigned long to user_uintptr_t. This
    will help user pointer (capability in PCuABI) in going downstream to the
    final handler.
    
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    (cherry picked from commit 49c9845e9561a4376184b86a5900f81058bb90d5)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- fs/filesystems.c
+++ fs/filesystems.c
@@ -189,7 +189,7 @@ static int fs_maxindex(void)
 /*
  * Whee.. Weird sysv syscall. 
  */
-SYSCALL_DEFINE3(sysfs, int, option, unsigned long, arg1, unsigned long, arg2)
+SYSCALL_DEFINE3(sysfs, int, option, user_uintptr_t, arg1, user_uintptr_t, arg2)
 {
 	int retval = -EINVAL;
 
