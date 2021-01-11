3a913f4a5c8106268048bab77df1cbe4f2d26b26
Amit Daniel Kachhap <amit.kachhap@arm.com>
2021-01-11 18:07:55 +0530

    kernel/signal: Use appropriate user pointer interfaces
    Use PCuABI friendly functions to convert addresses to capability pointers
    or vice-versa. Also use put_user_ptr while writing from user pointer.
    
    This is a minimal diff to get signal to build while waiting for PCuABI
    to be implemented for sigaltstack().
    
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    Co-developed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    (cherry picked from commit e8efa80b7a3c70fc817bbda1f4c93db2c2b9923e)
    [ Christian A. Ehrhardt: Conflict resolution ]
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -4392,7 +4392,8 @@ do_sigaltstack (const stack_t *ss, stack_t *oss, unsigned long sp,
 
 	if (oss) {
 		memset(oss, 0, sizeof(stack_t));
-		oss->ss_sp = (void __user *) t->sas_ss_sp;
+		/* TODO [PCuABI] - preserving user-provided capability */
+		oss->ss_sp = uaddr_to_user_ptr(t->sas_ss_sp);
 		oss->ss_size = t->sas_ss_size;
 		oss->ss_flags = sas_ss_flags(sp) |
 			(current->sas_ss_flags & SS_FLAG_BITS);
@@ -4469,7 +4470,7 @@ int restore_altstack(const stack_t __user *uss)
 int __save_altstack(stack_t __user *uss, unsigned long sp)
 {
 	struct task_struct *t = current;
-	int err = __put_user((void __user *)t->sas_ss_sp, &uss->ss_sp) |
+	int err = __put_user_ptr(uaddr_to_user_ptr(t->sas_ss_sp), &uss->ss_sp) |
 		__put_user(t->sas_ss_flags, &uss->ss_flags) |
 		__put_user(t->sas_ss_size, &uss->ss_size);
 	return err;
