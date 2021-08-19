b70fd12aa4b4697c7038559cc81ac1087baf8f50
Beata Michalska <beata.michalska@arm.com>
2021-08-19 16:40:45 +0100

    mm/mprotect: Enable basic capability support
    Add basic support for capabilities: at this point it is purely enabling
    handling capabilities without actually validating/verifying them.
    
    Signed-off-by: Beata Michalska <beata.michalska@arm.com>
    (cherry picked from commit 2c621b60ecc0bc0e85f1089f5ce9a3cd84b0da9a)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -798,7 +798,7 @@ fail:
 /*
  * pkey==-1 when doing a legacy mprotect()
  */
-static int do_mprotect_pkey(unsigned long start, size_t len,
+static int do_mprotect_pkey(user_uintptr_t start, size_t len,
 		unsigned long prot, int pkey)
 {
 	unsigned long nstart, end, tmp, reqprot;
@@ -810,6 +810,7 @@ static int do_mprotect_pkey(unsigned long start, size_t len,
 	struct mmu_gather tlb;
 	struct vma_iterator vmi;
 
+	/* TODO [PCuABI] - capability checks for uaccess */
 	start = untagged_addr(start);
 
 	prot &= ~(PROT_GROWSDOWN|PROT_GROWSUP);
@@ -945,7 +946,7 @@ out:
 	return error;
 }
 
-SYSCALL_DEFINE3(mprotect, unsigned long, start, size_t, len,
+SYSCALL_DEFINE3(mprotect, user_uintptr_t, start, size_t, len,
 		unsigned long, prot)
 {
 	return do_mprotect_pkey(start, len, prot, -1);
@@ -953,7 +954,7 @@ SYSCALL_DEFINE3(mprotect, unsigned long, start, size_t, len,
 
 #ifdef CONFIG_ARCH_HAS_PKEYS
 
-SYSCALL_DEFINE4(pkey_mprotect, unsigned long, start, size_t, len,
+SYSCALL_DEFINE4(pkey_mprotect, user_uintptr_t, start, size_t, len,
 		unsigned long, prot, int, pkey)
 {
 	return do_mprotect_pkey(start, len, prot, pkey);
