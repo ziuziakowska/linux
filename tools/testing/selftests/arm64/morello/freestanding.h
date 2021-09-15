9e9b4d9e7e6425972c6041ad26502b0d9a76f90a
Boyan Karatotev <boyan.karatotev@arm.com>
2021-09-15 12:24:02 +0100

    kselftests/arm64: Add a freestanding test harness
    The test harness is very helpful for testing but porting it to a
    freestanding environment is infeasible. Implement a minimal clone to use
    until a libc can boot the harness.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    (cherry picked from commit 6f6fc4836e281b62b0d3369a6e0286dbd84787fe)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- tools/testing/selftests/arm64/morello/freestanding.h
+++ tools/testing/selftests/arm64/morello/freestanding.h
@@ -14,6 +14,10 @@
 typedef __kernel_ssize_t ssize_t;
 typedef __kernel_pid_t pid_t;
 
+struct __test_meta {
+	int message;
+};
+
 void install_kernel_stack(void);
 uintcap_t __syscall(uintcap_t, uintcap_t, uintcap_t, uintcap_t, uintcap_t, uintcap_t, uintcap_t);
 int __attribute__ ((format(printf, 1, 2))) printf(const char *fmt, ...);
@@ -24,7 +28,64 @@ int __attribute__ ((format(printf, 1, 2))) printf(const char *fmt, ...);
 	__syscall(sys_no, __cap(arg1), __cap(arg2), __cap(arg3), __cap(arg4), __cap(arg5), __cap(arg6))
 #define syscall(sys_no, ...) __syscall_fill(sys_no, ##__VA_ARGS__, 0, 0, 0, 0, 0, 0)
 
+#define TH_LOG(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+#define __TH_LOG_ERROR(fmt, ...) \
+	TH_LOG("ERROR: %s:%d: " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+
+/* mimic the kselftest harness */
+#define TEST(test_name) \
+	void test_name##_actual(void); \
+	void test_name(void) \
+	{ \
+		struct __test_meta meta = {.message = 0};\
+		__cur_test = &meta; \
+		test_name##_actual(); \
+		TH_LOG("PASSED: %s", #test_name); \
+	} \
+	void test_name##_actual(void)
+
+/* this macro emulates its harness counterpart but is not API compatible */
+#define __EXPECT(exp, seen, op, exit_on_fail) \
+	do { \
+		if (!((exp) op (seen))) { \
+			__TH_LOG_ERROR("'(%s) %s (%s)' was false", #exp, #op, #seen); \
+			__cur_test->message = 1; \
+		} \
+	} while (0); \
+	for (; __cur_test->message; __cur_test->message = (exit_on_fail) ? (exit(1), 1) : 0)
+
+/*
+ * WARNING: All of the following macros work at any point in the call stack of
+ * a TEST context. Their kselftest_harness equivalents DO NOT. If compatibility
+ * with the harness is desired, they should only be used in a top-level TEST
+ * context
+ */
+#define EXPECT(exp, seen, op) __EXPECT(exp, seen, op, 0)
+#define ASSERT(exp, seen, op) __EXPECT(exp, seen, op, 1)
+
+#define ASSERT_NE(exp, seen) ASSERT(exp, seen, !=)
+#define ASSERT_EQ(exp, seen) ASSERT(exp, seen, ==)
+#define ASSERT_LT(exp, seen) ASSERT(exp, seen, <)
+#define ASSERT_LE(exp, seen) ASSERT(exp, seen, <=)
+#define ASSERT_GT(exp, seen) ASSERT(exp, seen, >)
+#define ASSERT_GE(exp, seen) ASSERT(exp, seen, >=)
+#define ASSERT_TRUE(seen) ASSERT(0, seen, !=)
+#define ASSERT_FALSE(seen) ASSERT(0, seen, ==)
+#define ASSERT_NULL(seen) ASSERT(NULL, seen, ==)
+
+#define EXPECT_NE(exp, seen) EXPECT(exp, seen, !=)
+#define EXPECT_EQ(exp, seen) EXPECT(exp, seen, ==)
+#define EXPECT_LT(exp, seen) EXPECT(exp, seen, <)
+#define EXPECT_LE(exp, seen) EXPECT(exp, seen, <=)
+#define EXPECT_GT(exp, seen) EXPECT(exp, seen, >)
+#define EXPECT_GE(exp, seen) EXPECT(exp, seen, >=)
+#define EXPECT_TRUE(seen) EXPECT(0, seen, !=)
+#define EXPECT_FALSE(seen) EXPECT(0, seen, ==)
+#define EXPECT_NULL(seen) EXPECT(NULL, seen, ==)
+
+
 extern void *stack_from_kernel;
+extern struct __test_meta *__cur_test;
 
 static inline size_t strlen(const char *str)
 {
