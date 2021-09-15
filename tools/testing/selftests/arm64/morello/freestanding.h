2192f65ffa7c504ef48d201a99adc475dfc79466
Boyan Karatotev <boyan.karatotev@arm.com>
2021-09-15 12:24:00 +0100

    kselftests/arm64: Implement a minimal stdlib
    The freestanding environment doesn't have any libc, but a lot of its
    functionality is desired. Add some of it.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    (cherry picked from commit f82e77593312a46528df229b5ab0c9bda6c4bb82)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- tools/testing/selftests/arm64/morello/freestanding.h
+++ tools/testing/selftests/arm64/morello/freestanding.h
@@ -7,10 +7,12 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <asm/unistd.h>
 #include <linux/posix_types.h>
 
 /* this is provided by libc, so roll our own */
 typedef __kernel_ssize_t ssize_t;
+typedef __kernel_pid_t pid_t;
 
 void install_kernel_stack(void);
 uintcap_t __syscall(uintcap_t, uintcap_t, uintcap_t, uintcap_t, uintcap_t, uintcap_t, uintcap_t);
@@ -28,6 +30,28 @@ static inline size_t strlen(const char *str)
 	size_t len = 0;
 	for (; str[len] != '\0'; len++);
 	return len;
+}
+
+static inline int memcmp(const char *buf1, const char *buf2, size_t len)
+{
+	for (; len != 0; len--, buf1++, buf2++) {
+		if (*buf1 != *buf2)
+			return (*buf1 < *buf2) ? -1 : 1;
+	}
+	return 0;
+}
+
+static inline void __attribute__ ((noreturn)) exit(int status)
+{
+	/* behave as musl */
+	syscall(__NR_exit_group, status);
+	syscall(__NR_exit, status);
+	for (;;);
+}
+
+static inline ssize_t write(int fd, const void *buf, size_t count)
+{
+	return syscall(__NR_write, fd, buf, count);
 }
 
 #endif
