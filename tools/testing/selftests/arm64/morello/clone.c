bd5c7f7674c75ef21deae831c462afac659689d4
Victor Do Nascimento <victor.donascimento@arm.com>
2022-02-03 00:46:11 +0000

    kselftests/arm64: morello: Add basic wait test
    Add very basic test cases for waitid ensuring correct behaviour
    and capability support for struct siginfo and struct rusage pointer
    args.
    
    Signed-off-by: Victor Do Nascimento <victor.donascimento@arm.com>
    Co-developed-by: Beata Michalska <beata.michalska@arm.com>
    Signed-off-by: Beata Michalska <beata.michalska@arm.com>
    (cherry picked from commit 0a36a80020c5531774ac30ca33c7d69029fb0ace)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- tools/testing/selftests/arm64/morello/clone.c
+++ tools/testing/selftests/arm64/morello/clone.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (C) 2021  Arm Limited
 #include <linux/mman.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
 #include <linux/sched.h>
 #include <errno.h>
 #include <cheriintrin.h>
@@ -22,8 +24,9 @@
 /* Test flags */
 #define CLONE_TH_RESTRICTED	BIT(1)
 #define CLONE_TH_TLS		BIT(2)
+#define CLONE_TH_RUSAGE		BIT(3)
 
-struct clone_fixture {
+struct test_fixture {
 	int status;
 	int flags;
 	void *sp;
@@ -45,7 +48,7 @@ static void probe_addr_range(uintcap_t start_addr, size_t size, int interval)
 
 static int clone_base_fn(void *data)
 {
-	struct clone_fixture *__data = (struct clone_fixture *)data;
+	struct test_fixture *__data = (struct test_fixture *)data;
 	void *addr = __builtin_frame_address(0);
 	size_t size = STACK_SIZE;
 
@@ -82,7 +85,7 @@ done:
 }
 
 static inline __attribute__((always_inline))
-void run_single(struct clone_fixture *data)
+void clone_single(struct test_fixture *data)
 {
 	int ppid = 0, cpid = 0;
 	int result = -EINVAL;
@@ -133,30 +136,123 @@ leave:
 		munmap(tls, STACK_SIZE >> 10);
 }
 
-#define RUN_WITH_FIXTURE(name, th_flags)		\
-	struct clone_fixture name = {			\
-		.flags = th_flags,			\
+static int sleep_cycles(void *n_cycle)
+{
+	int __n_cycle = *((int *) n_cycle);
+
+	for (int i = 0; i < __n_cycle; i += 1)
+		asm("mov xzr, #1");
+	return 0;
+}
+
+/* The wait_single test makes use of the fact that both wait4 and
+ * waitid invoke do_wait(&wait_opts), both returning resource
+ * usage for the child in *ru with waitid additionally recording
+ * information about the child's change in state.
+ * Therefore if waitid works, we can be confident of wait4's
+ * correctness.
+ */
+static inline __attribute__((always_inline))
+void wait_single(struct test_fixture *data)
+{
+	void *new_stack = mmap_verified(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
+				 MAP_ANONYMOUS | MAP_PRIVATE, -1, 0,
+				 STACK_REQ_PERMS);
+
+	pid_t cpid = -EINVAL;
+	pid_t result = -EINVAL;
+	int n_cycle = 1000;
+	struct rusage *wait_ru = data->flags & CLONE_TH_RUSAGE ?
+			&(struct rusage){0} : NULL;
+	siginfo_t *wait_si = &(siginfo_t){0};
+
+	int clone_flags = SIGCHLD;
+
+	ASSERT_NE(new_stack, NULL);
+
+	cpid = __clone(sleep_cycles, (uintcap_t)new_stack + STACK_SIZE, clone_flags,
+			&n_cycle, NULL, NULL, NULL);
+
+	ASSERT_GT(cpid, 0) {
+		__TH_LOG_ERROR("wait_test: Failed to clone");
+	}
+
+	result = syscall(__NR_waitid, P_PID, cpid, wait_si, WEXITED, wait_ru);
+
+	ASSERT_EQ(result, 0) {
+		__TH_LOG_ERROR("wait_test: Failed on wait");
+	}
+
+	/*
+	 * Validate (to a limited extent) both struct rusage and siginfo_t.
+	 * Might explode if capability tag gets cleared on the way...
+	 */
+	if (data->flags & CLONE_TH_RUSAGE) {
+		int nonzero_count = 0;
+
+		for (unsigned int i = 0; i < sizeof(*wait_ru); ++i)
+			if (!!((unsigned char *)wait_ru)[i])
+				++nonzero_count;
+		EXPECT_GT(nonzero_count, 0) {
+			TH_LOG("Empty struct rusage on return from waitid");
+			goto end;
+		}
+	}
+
+	/*
+	 * si_signo is supposed to be always set to SIGCHLD (when successfully
+	 * identifying the child process).
+	 */
+	EXPECT_EQ(wait_si->si_signo, SIGCHLD) {
+		TH_LOG("Unexpected si_signo value received: %d, expected SIGCHLD(%d)",
+		       wait_si->si_signo, SIGCHLD);
+		goto end;
+	}
+
+	EXPECT_EQ(wait_si->si_pid, cpid) {
+		TH_LOG("Unexpected child's process ID. Got %d, expected %d",
+			wait_si->si_pid, cpid);
+		goto end;
+	}
+
+end:
+	munmap(new_stack, STACK_SIZE);
+}
+
+#define RUN_WITH_FIXTURE(name, run_flags, fn)		\
+	struct test_fixture name = {			\
+		.flags = run_flags,			\
 	};		 				\
-	run_single(&name)
+	fn(&name)
 
 TEST(test_clone)
 {
-	RUN_WITH_FIXTURE(data, 0);
+	RUN_WITH_FIXTURE(data, 0,  clone_single);
 }
 
 TEST(test_clone_tls)
 {
-	RUN_WITH_FIXTURE(data, CLONE_TH_TLS);
+	RUN_WITH_FIXTURE(data, CLONE_TH_TLS, clone_single);
 }
 
 TEST(test_clone_restricted)
 {
-	RUN_WITH_FIXTURE(data, CLONE_TH_RESTRICTED);
+	RUN_WITH_FIXTURE(data, CLONE_TH_RESTRICTED, clone_single);
 }
 
 TEST(test_clone_tls_restricted)
 {
-	RUN_WITH_FIXTURE(data, CLONE_TH_TLS | CLONE_TH_RESTRICTED);
+	RUN_WITH_FIXTURE(data, CLONE_TH_TLS | CLONE_TH_RESTRICTED, clone_single);
+}
+
+TEST(test_wait_raw)
+{
+	RUN_WITH_FIXTURE(data, 0, wait_single);
+}
+
+TEST(test_wait_rusage)
+{
+	RUN_WITH_FIXTURE(data, CLONE_TH_RUSAGE, wait_single);
 }
 
 void run_restricted(uintcap_t entry_point)
@@ -191,5 +287,7 @@ int main(void)
 	run_restricted((uintcap_t)test_clone_restricted);
 	test_clone_tls();
 	run_restricted((uintcap_t)test_clone_tls_restricted);
+	test_wait_raw();
+	test_wait_rusage();
 	return 0;
 }
