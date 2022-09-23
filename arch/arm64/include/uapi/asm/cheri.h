24380df4397cc48a7511f765a1564843ef955f60
Kevin Brodsky <kevin.brodsky@arm.com>
2022-09-23 10:13:21 +0100

    arm64: uapi: Add asm/cheri.h
    Add a new uapi header to define generic CHERI concepts with
    arm64-specific values.
    
    To start with, provide a macro representing the VMem software
    permission, as defined in the PCuABI specification. The macro name
    matches CheriBSD, facilitating its use in a (somewhat) OS-agnostic
    way.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    (cherry picked from commit d9dc13aba7e27bde3693101953b95e2235e5f874)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

---
+++ arch/arm64/include/uapi/asm/cheri.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI__ASM_CHERI_H
+#define _UAPI__ASM_CHERI_H
+
+/*
+ * VMem software-defined capability permission, assigned to the User[0]
+ * permission on Morello (bit 2).
+ */
+#define CHERI_PERM_SW_VMEM	(1 << 2)
+
+#endif /* _UAPI__ASM_CHERI_H */
