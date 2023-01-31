3fcc1a622c5c8fd8642464cbff0449f5bb877cf6
Kevin Brodsky <kevin.brodsky@arm.com>
2023-01-31 14:56:28 +0000

    arm64: signal: Avoid unnecessary capability arithmetic
    sp_top is only used to compute the size to provide to access_ok().
    It should therefore be just an address, not a capability (in
    PCuABI). GCC rightly warns about this situation, as subtracting two
    capabilities is ambiguous (it may be interpreted as a capability
    operation subtracting the address of the LHS with the address of the
    RHS, while here we expect a simple integer subtraction).
    
    Fixes: ("linux/sched/signal.h: Modify the stack pointer to user_uintptr_t")
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    (cherry picked from commit d66f56cced0f1b86f8b72da8770f268e8600cab4)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -1501,7 +1501,8 @@ static int setup_sigframe(struct rt_sigframe_user_layout *user,
 static int get_sigframe(struct rt_sigframe_user_layout *user,
 			 struct ksignal *ksig, struct pt_regs *regs)
 {
-	user_uintptr_t sp, sp_top;
+	user_uintptr_t sp;
+	ptraddr_t sp_top;
 	int err;
 
 	init_user_layout(user);
@@ -1509,7 +1510,8 @@ static int get_sigframe(struct rt_sigframe_user_layout *user,
 	if (err)
 		return err;
 
-	sp = sp_top = sigsp(signal_sp(regs), ksig);
+	sp = sigsp(signal_sp(regs), ksig);
+	sp_top = (ptraddr_t)sp;
 
 	sp = round_down(sp - sizeof(struct frame_record), 16);
 	user->next_frame = (struct frame_record __user *)sp;
