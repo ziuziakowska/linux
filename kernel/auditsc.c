38514d5f8311fdbdd7d01c6667d718566dcd07a3
Kevin Brodsky <kevin.brodsky@arm.com>
2023-03-17 07:51:15 +0000

    audit: Create appropriate user pointer for uaccess
    audit_log_execve_info() wants to read the program's arguments
    directly from user memory; to perform the uaccess we therefore need
    to create a valid user pointer from the address of the arguments.
    
    uaddr_to_user_ptr_safe() should no longer be used for that purpose.
    Instead, we use make_user_ptr_for_read_uaccess() to create a user
    pointer with appropriate bounds and permissions (in PCuABI).
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    (cherry picked from commit a809ca4af4ece1c2fb292d418c95b5f1f4c30a7f)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -1132,7 +1132,8 @@ static void audit_log_execve_info(struct audit_context *context,
 	unsigned int arg;
 	char *buf_head;
 	char *buf;
-	const char __user *p = uaddr_to_user_ptr_safe(current->mm->arg_start);
+	const char __user *p = make_user_ptr_for_read_uaccess(
+		current->mm->arg_start, MAX_EXECVE_AUDIT_LEN);
 
 	/* NOTE: this buffer needs to be large enough to hold all the non-arg
 	 *       data we put in the audit record for this argument (see the
