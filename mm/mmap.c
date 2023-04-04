a2674280ff5d2ae8cb9177f405c44dbd5b3cd412
Amit Daniel Kachhap <amitdaniel.kachhap@arm.com>
2023-04-04 15:18:37 +0530

    mm/mmap: Disable MAP_GROWSDOWN mapping flag for PCuABI
    The MAP_GROWSDOWN flag is not supported by PCuABI specification.
    Hence, reject such requests with -EOPNOTSUPP error.
    
    Signed-off-by: Amit Daniel Kachhap <amitdaniel.kachhap@arm.com>
    (cherry picked from commit f56b8f9587cff369c951f57fc165921ad8f27800)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -646,11 +646,17 @@ user_uintptr_t ksys_mmap_pgoff(user_uintptr_t user_ptr, unsigned long len,
 	 * - The PCuABI reservation model introduces the concept of maximum
 	 *   protection mappings can have. Add a check to make sure the
 	 *   requested protection does not exceed the maximum protection.
+	 * - MAP_GROWSDOWN mappings have no fixed bounds and hence are not
+	 *   supported in the PCuABI reservation model.
 	 */
 	if (reserv_is_supported(current->mm)) {
 		if ((PROT_MAX_EXTRACT(prot) != 0) &&
 		    ((PROT_EXTRACT(prot) & PROT_MAX_EXTRACT(prot)) != PROT_EXTRACT(prot)))
 			goto out_fput;
+		if (flags & MAP_GROWSDOWN) {
+			retval = -EOPNOTSUPP;
+			goto out_fput;
+		}
 	}
 	retval = check_pcuabi_map_ptr_arg(user_ptr, len, flags & MAP_FIXED, false);
 	if (retval)
