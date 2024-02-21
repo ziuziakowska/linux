a379985d1fa01b0a4fa001279a2ecebefbfa9527
Amit Daniel Kachhap <amitdaniel.kachhap@arm.com>
2024-02-21 12:05:09 +0530

    arm64: user_ptr: Implement Morello capability permission helper
    Implement arch_user_ptr_owning_perms_from_prot() in PCuABI to handle
    Morello-specific flags and permissions. The hook will be used in a
    subsequent patch to restrict the permissions of owning capabilities.
    
    Signed-off-by: Amit Daniel Kachhap <amitdaniel.kachhap@arm.com>
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    (cherry picked from commit eb28f03d489a4895fb9163964c4a703be29a973c)
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

---
+++ arch/arm64/include/asm/user_ptr.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ASM_USER_PTR_H
+#define __ASM_USER_PTR_H
+
+#include <linux/cheri.h>
+#include <linux/mman.h>
+#include <linux/sched/task_stack.h>
+#include <asm/processor.h>
+
+#ifdef CONFIG_CHERI_PURECAP_UABI
+
+static inline
+user_ptr_perms_t arch_user_ptr_owning_perms_from_prot(int prot, unsigned long vm_flags)
+{
+	struct pt_regs *regs = task_pt_regs(current);
+	cheri_perms_t perms = 0;
+
+	if ((prot & PROT_READ) && (vm_flags & VM_READ_CAPS))
+		perms |= ARM_CAP_PERMISSION_MUTABLE_LOAD;
+
+	if (prot & PROT_EXEC) {
+		if (cheri_perms_get(regs->pcc) & CHERI_PERM_SYSTEM_REGS)
+			perms |= CHERI_PERM_SYSTEM_REGS;
+		if (cheri_perms_get(regs->pcc) & ARM_CAP_PERMISSION_EXECUTIVE)
+			perms |= ARM_CAP_PERMISSION_EXECUTIVE;
+	}
+
+	return perms;
+}
+#define arch_user_ptr_owning_perms_from_prot arch_user_ptr_owning_perms_from_prot
+
+#endif /* CONFIG_CHERI_PURECAP_UABI */
+
+#endif /* __ASM_USER_PTR_H */
