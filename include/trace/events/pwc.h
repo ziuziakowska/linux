d5186d267dcf90d3de119da0ccaece78843b607a
Martin Kaiser <martin.kaiser@codasip.com>
2025-10-27 16:23:10 +0100

    pwc: cheri: convert events
    Convert the events for cheri. Use macros for pointer handling.
    This was tested using trace event injection.
    
    Signed-off-by: Martin Kaiser <martin.kaiser@codasip.com>

--- include/trace/events/pwc.h
+++ include/trace/events/pwc.h
@@ -12,27 +12,27 @@ TRACE_EVENT(pwc_handler_enter,
 	TP_PROTO(struct urb *urb, struct pwc_device *pdev),
 	TP_ARGS(urb, pdev),
 	TP_STRUCT__entry(
-		__field(struct urb*, urb)
-		__field(struct pwc_frame_buf*, fbuf)
+		__ptr(struct urb*, urb)
+		__ptr(struct pwc_frame_buf*, fbuf)
 		__field(int, urb__status)
 		__field(u32, urb__actual_length)
 		__field(int, fbuf__filled)
 		__string(name, pdev->v4l2_dev.name)
 	),
 	TP_fast_assign(
-		__entry->urb = urb;
-		__entry->fbuf = pdev->fill_buf;
+		__assign_ptr(urb, urb);
+		__assign_ptr(fbuf, pdev->fill_buf);
 		__entry->urb__status = urb->status;
 		__entry->urb__actual_length = urb->actual_length;
 		__entry->fbuf__filled = (pdev->fill_buf
 					 ? pdev->fill_buf->filled : 0);
 		__assign_str(name);
 	),
-	TP_printk("dev=%s (fbuf=%p filled=%d) urb=%p (status=%d actual_length=%u)",
+	TP_printk("dev=%s (fbuf=" TRACE_CAP_FMT " filled=%d) urb=" TRACE_CAP_FMT " (status=%d actual_length=%u)",
 		__get_str(name),
-		__entry->fbuf,
+		__get_cap(fbuf),
 		__entry->fbuf__filled,
-		__entry->urb,
+		__get_cap(urb),
 		__entry->urb__status,
 		__entry->urb__actual_length)
 );
@@ -41,22 +41,22 @@ TRACE_EVENT(pwc_handler_exit,
 	TP_PROTO(struct urb *urb, struct pwc_device *pdev),
 	TP_ARGS(urb, pdev),
 	TP_STRUCT__entry(
-		__field(struct urb*, urb)
-		__field(struct pwc_frame_buf*, fbuf)
+		__ptr(struct urb*, urb)
+		__ptr(struct pwc_frame_buf*, fbuf)
 		__field(int, fbuf__filled)
 		__string(name, pdev->v4l2_dev.name)
 	),
 	TP_fast_assign(
-		__entry->urb = urb;
-		__entry->fbuf = pdev->fill_buf;
+		__assign_ptr(urb, urb);
+		__assign_ptr(fbuf, pdev->fill_buf);
 		__entry->fbuf__filled = pdev->fill_buf->filled;
 		__assign_str(name);
 	),
-	TP_printk(" dev=%s (fbuf=%p filled=%d) urb=%p",
+	TP_printk(" dev=%s (fbuf=" TRACE_CAP_FMT " filled=%d) urb=" TRACE_CAP_FMT "",
 		__get_str(name),
-		__entry->fbuf,
+		__get_cap(fbuf),
 		__entry->fbuf__filled,
-		__entry->urb)
+		__get_cap(urb))
 );
 
 #endif /* _TRACE_PWC_H */
