b3490dc69af2edd56e4e0cd058d269fc7f27c1b2
Martin Kaiser <martin.kaiser@codasip.com>
2025-10-27 16:19:49 +0100

    napi: cheri: convert events
    Convert the events for cheri. Use macros for pointer handling.

--- include/trace/events/napi.h
+++ include/trace/events/napi.h
@@ -18,21 +18,21 @@ TRACE_EVENT(napi_poll,
 	TP_ARGS(napi, work, budget),
 
 	TP_STRUCT__entry(
-		__field(	struct napi_struct *,	napi)
+		__ptr(		struct napi_struct *,	napi)
 		__string(	dev_name, napi->dev ? napi->dev->name : NO_DEV)
 		__field(	int,			work)
 		__field(	int,			budget)
 	),
 
 	TP_fast_assign(
-		__entry->napi = napi;
+		__assign_ptr(napi, napi);
 		__assign_str(dev_name);
 		__entry->work = work;
 		__entry->budget = budget;
 	),
 
-	TP_printk("napi poll on napi struct %p for device %s work %d budget %d",
-		  __entry->napi, __get_str(dev_name),
+	TP_printk("napi poll on napi struct " TRACE_CAP_FMT " for device %s work %d budget %d",
+		  __get_cap(napi), __get_str(dev_name),
 		  __entry->work, __entry->budget)
 );
 
