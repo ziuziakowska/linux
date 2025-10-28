6cc788f2d5d6daba0ae3313c3f1d270aa4558625
Martin Kaiser <martin.kaiser@codasip.com>
2025-10-28 17:55:41 +0100

    mac80211: cheri: convert events
    Convert the events for cheri.
    
    This had to be done manually. The only components with pointers are
    hidden in defines (and not printed out).

--- net/mac80211/trace.h
+++ net/mac80211/trace.h
@@ -28,10 +28,10 @@
 #define STA_PR_FMT	" sta:%pM"
 #define STA_PR_ARG	__entry->sta_addr
 
-#define VIF_ENTRY	__field(enum nl80211_iftype, vif_type) __field(void *, sdata)	\
+#define VIF_ENTRY	__field(enum nl80211_iftype, vif_type) __ptr(void *, sdata)	\
 			__field(bool, p2p)						\
 			__string(vif_name, sdata->name)
-#define VIF_ASSIGN	__entry->vif_type = sdata->vif.type; __entry->sdata = sdata;	\
+#define VIF_ASSIGN	__entry->vif_type = sdata->vif.type; __assign_ptr(sdata, sdata);	\
 			__entry->p2p = sdata->vif.p2p;					\
 			__assign_str(vif_name)
 #define VIF_PR_FMT	" vif:%s(%d%s)"
