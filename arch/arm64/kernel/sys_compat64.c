ef3cb05e1a1b08e12447ed568fb63a018fa42396
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2025-11-14 14:34:59 +0100

    cheri: arm64: compat: Use correct syscall handlers
    When doing 64-bit compat for cheri (as opposed to 32-bit
    compat) several system calls do not need a compat version
    because they only deal with 32-bit vs. 64-bit unsigned long.
    They still need compat entry code, though. If this is the case
    the handle that the compat entry code jumps to is re-defined
    to point to the 64-bit syscall.
    
    After rebasing to v6.16 there is a 32-bit syscall table and a 64-bit
    one and for the 64-bit compat code we use compat entries in the
    64-bit table. All the time related syscalls do not have/need a
    compat version in the 64-bit table anymore and thus there is no
    need to re-define them.
    
    Actually call the compat version of the syscall from the compat
    entry code when using the 64-bit table and remove the defines
    that are no longer necessary as a result of this.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- arch/arm64/kernel/sys_compat64.c
+++ arch/arm64/kernel/sys_compat64.c
@@ -19,14 +19,11 @@
  * The following compat handlers are not defined in generic code, but the
  * native handlers work for 64-bit compat.
  */
-#define __arm64_compatentry_compat_sys_truncate64		__arm64_compatentry_sys_truncate
-#define __arm64_compatentry_compat_sys_ftruncate64		__arm64_compatentry_sys_ftruncate
 #define __arm64_compatentry_compat_sys_fallocate		__arm64_compatentry_sys_fallocate
 #define __arm64_compatentry_compat_sys_pread64			__arm64_compatentry_sys_pread64
 #define __arm64_compatentry_compat_sys_pwrite64			__arm64_compatentry_sys_pwrite64
 #define __arm64_compatentry_compat_sys_sync_file_range		__arm64_compatentry_sys_sync_file_range
 #define __arm64_compatentry_compat_sys_readahead		__arm64_compatentry_sys_readahead
-#define __arm64_compatentry_compat_sys_fadvise64_64		__arm64_compatentry_sys_fadvise64_64
 
 /*
  * The compat_sys_{preadv,pwritev}{,2} handlers are not appropriate for 64-bit
@@ -39,56 +36,8 @@
 #define __arm64_compatentry_compat_sys_pwritev2			__arm64_compatentry_sys_pwritev2
 
 /*
- * 64-bit tasks use mmap (not mmap2).
- */
-#define __arm64_compatentry_sys_mmap2				__arm64_compatentry_sys_mmap
-
-/*
- * 64-bit tasks use lseek (not llseek).
- */
-#define __arm64_compatentry_sys_llseek				__arm64_compatentry_compat_sys_lseek
-
-/*
- * Use the same 'struct stat' as native AArch64.
- */
-#define __arm64_compatentry_sys_fstatat64			__arm64_compatentry_sys_newfstatat
-#define __arm64_compatentry_sys_fstat64				__arm64_compatentry_sys_newfstat
-
-/*
- * 64-bit tasks use statfs (not statfs64).
- */
-#define __arm64_compatentry_compat_sys_statfs64			__arm64_compatentry_compat_sys_statfs
-#define __arm64_compatentry_compat_sys_fstatfs64		__arm64_compatentry_compat_sys_fstatfs
-
-/*
  * 64-bit tasks pass 64-bit time values (not 32-bit).
  */
-#define __arm64_compatentry_sys_io_getevents_time32		__arm64_compatentry_sys_io_getevents
-#define __arm64_compatentry_compat_sys_pselect6_time32		__arm64_compatentry_compat_sys_pselect6_time64
-#define __arm64_compatentry_compat_sys_ppoll_time32		__arm64_compatentry_compat_sys_ppoll_time64
-#define __arm64_compatentry_sys_timerfd_settime32		__arm64_compatentry_sys_timerfd_settime
-#define __arm64_compatentry_sys_timerfd_gettime32		__arm64_compatentry_sys_timerfd_gettime
-#define __arm64_compatentry_sys_utimensat_time32		__arm64_compatentry_sys_utimensat
-#define __arm64_compatentry_sys_futex_time32			__arm64_compatentry_sys_futex
-#define __arm64_compatentry_sys_nanosleep_time32		__arm64_compatentry_sys_nanosleep
-#define __arm64_compatentry_sys_timer_gettime32			__arm64_compatentry_sys_timer_gettime
-#define __arm64_compatentry_sys_timer_settime32			__arm64_compatentry_sys_timer_settime
-#define __arm64_compatentry_sys_clock_settime32			__arm64_compatentry_sys_clock_settime
-#define __arm64_compatentry_sys_clock_gettime32			__arm64_compatentry_sys_clock_gettime
-#define __arm64_compatentry_sys_clock_getres_time32		__arm64_compatentry_sys_clock_getres
-#define __arm64_compatentry_sys_clock_nanosleep_time32		__arm64_compatentry_sys_clock_nanosleep
-#define __arm64_compatentry_sys_sched_rr_get_interval_time32	__arm64_compatentry_sys_sched_rr_get_interval
-#define __arm64_compatentry_compat_sys_rt_sigtimedwait_time32	__arm64_compatentry_compat_sys_rt_sigtimedwait_time64
-#define __arm64_compatentry_compat_sys_gettimeofday		__arm64_compatentry_sys_gettimeofday
-#define __arm64_compatentry_compat_sys_settimeofday		__arm64_compatentry_sys_settimeofday
-#define __arm64_compatentry_sys_adjtimex_time32			__arm64_compatentry_sys_adjtimex
-#define __arm64_compatentry_sys_mq_timedsend_time32		__arm64_compatentry_sys_mq_timedsend
-#define __arm64_compatentry_sys_mq_timedreceive_time32		__arm64_compatentry_sys_mq_timedreceive
-#define __arm64_compatentry_sys_semtimedop_time32		__arm64_compatentry_sys_semtimedop
-#define __arm64_compatentry_compat_sys_recvmmsg_time32		__arm64_compatentry_compat_sys_recvmmsg_time64
-#define __arm64_compatentry_compat_sys_wait4			__arm64_compatentry_sys_wait4
-#define __arm64_compatentry_sys_clock_adjtime32			__arm64_compatentry_sys_clock_adjtime
-#define __arm64_compatentry_compat_sys_io_pgetevents		__arm64_compatentry_compat_sys_io_pgetevents_time64
 #define __arm64_compatentry_compat_sys_getitimer		__arm64_compatentry_sys_getitimer
 #define __arm64_compatentry_compat_sys_setitimer		__arm64_compatentry_sys_setitimer
 #define __arm64_compatentry_compat_sys_getrusage		__arm64_compatentry_sys_getrusage
@@ -103,7 +52,7 @@ asmlinkage long __arm64_compatentry_sys_ni_syscall(const struct pt_regs *__unuse
 
 #define __SYSCALL_RETPTR __SYSCALL
 #define __SYSCALL_WITH_COMPAT_RETPTR __SYSCALL_WITH_COMPAT
-#define __SYSCALL_WITH_COMPAT(nr, native, compat)  __SYSCALL(nr, native)
+#define __SYSCALL_WITH_COMPAT(nr, native, compat)  __SYSCALL(nr, compat)
 
 #undef __SYSCALL
 #define __SYSCALL(nr, sym)	asmlinkage long __arm64_compatentry_##sym(const struct pt_regs *);
