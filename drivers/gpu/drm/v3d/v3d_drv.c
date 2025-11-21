9b5d5b27ef206ed111024d75118085fde464c1cc
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2025-11-21 17:10:10 +0100

    cheri: compat64: drm: v3d: Enable compat64 handling
    Add compat64 handling for the ioctls in the v3d DRM driver.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- drivers/gpu/drm/v3d/v3d_drv.c
+++ drivers/gpu/drm/v3d/v3d_drv.c
@@ -27,6 +27,7 @@
 #include <drm/drm_managed.h>
 #include <drm/drm_print.h>
 #include <uapi/drm/v3d_drm.h>
+#include <drm/compat64_v3d_drm.h>
 
 #include "v3d_drv.h"
 #include "v3d_regs.h"
@@ -235,18 +236,23 @@ static const struct file_operations v3d_drm_fops = {
  * need to stay DRM_AUTH until we do buffer size/offset validation.
  */
 static const struct drm_ioctl_desc v3d_drm_ioctls[] = {
-	DRM_IOCTL_DEF_DRV(V3D_SUBMIT_CL, v3d_submit_cl_ioctl, DRM_RENDER_ALLOW | DRM_AUTH),
+	DRM_IOCTL_DEF_DRV_C64(V3D_SUBMIT_CL, v3d_submit_cl_ioctl,
+			      DRM_RENDER_ALLOW | DRM_AUTH, drm_v3d_submit_cl),
 	DRM_IOCTL_DEF_DRV(V3D_WAIT_BO, v3d_wait_bo_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_CREATE_BO, v3d_create_bo_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_MMAP_BO, v3d_mmap_bo_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_GET_PARAM, v3d_get_param_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_GET_BO_OFFSET, v3d_get_bo_offset_ioctl, DRM_RENDER_ALLOW),
-	DRM_IOCTL_DEF_DRV(V3D_SUBMIT_TFU, v3d_submit_tfu_ioctl, DRM_RENDER_ALLOW | DRM_AUTH),
-	DRM_IOCTL_DEF_DRV(V3D_SUBMIT_CSD, v3d_submit_csd_ioctl, DRM_RENDER_ALLOW | DRM_AUTH),
+	DRM_IOCTL_DEF_DRV_C64(V3D_SUBMIT_TFU, v3d_submit_tfu_ioctl,
+			      DRM_RENDER_ALLOW | DRM_AUTH, drm_v3d_submit_tfu),
+	DRM_IOCTL_DEF_DRV_C64(V3D_SUBMIT_CSD, v3d_submit_csd_ioctl,
+			      DRM_RENDER_ALLOW | DRM_AUTH, drm_v3d_submit_csd),
 	DRM_IOCTL_DEF_DRV(V3D_PERFMON_CREATE, v3d_perfmon_create_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_PERFMON_DESTROY, v3d_perfmon_destroy_ioctl, DRM_RENDER_ALLOW),
-	DRM_IOCTL_DEF_DRV(V3D_PERFMON_GET_VALUES, v3d_perfmon_get_values_ioctl, DRM_RENDER_ALLOW),
-	DRM_IOCTL_DEF_DRV(V3D_SUBMIT_CPU, v3d_submit_cpu_ioctl, DRM_RENDER_ALLOW | DRM_AUTH),
+	DRM_IOCTL_DEF_DRV_C64(V3D_PERFMON_GET_VALUES, v3d_perfmon_get_values_ioctl,
+			      DRM_RENDER_ALLOW, drm_v3d_perfmon_get_values),
+	DRM_IOCTL_DEF_DRV_C64(V3D_SUBMIT_CPU, v3d_submit_cpu_ioctl,
+			      DRM_RENDER_ALLOW | DRM_AUTH, drm_v3d_submit_cpu),
 	DRM_IOCTL_DEF_DRV(V3D_PERFMON_GET_COUNTER, v3d_perfmon_get_counter_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_PERFMON_SET_GLOBAL, v3d_perfmon_set_global_ioctl, DRM_RENDER_ALLOW),
 };
--- drivers/gpu/drm/v3d/v3d_submit.c
+++ drivers/gpu/drm/v3d/v3d_submit.c
@@ -11,6 +11,8 @@
 #include "v3d_regs.h"
 #include "v3d_trace.h"
 
+#include <drm/compat64_v3d_drm.h>
+
 /* Takes the reservation lock on all the BOs being referenced, so that
  * we can attach fences and update the reservations after pushing the job
  * to the queue.
@@ -387,7 +389,7 @@ v3d_get_multisync_submit_deps(struct drm_file *file_priv,
 		return -EINVAL;
 	}
 
-	if (copy_from_user_with_ptr(&multisync, ext, sizeof(multisync)))
+	if (__c64_copy_from_user_with_ptr(drm_v3d_multi_sync, &multisync, ext))
 		return -EFAULT;
 
 	if (multisync.pad)
@@ -440,7 +442,7 @@ v3d_get_cpu_indirect_csd_params(struct drm_file *file_priv,
 	if (!v3d_validate_cpu_job(file_priv, job))
 		return -EINVAL;
 
-	if (copy_from_user_with_ptr(&indirect_csd, ext, sizeof(indirect_csd)))
+	if (__c64_copy_from_user_with_ptr(drm_v3d_indirect_csd, &indirect_csd, ext))
 		return -EFAULT;
 
 	if (!v3d_has_csd(v3d)) {
@@ -476,7 +478,7 @@ v3d_get_cpu_timestamp_query_params(struct drm_file *file_priv,
 	if (!v3d_validate_cpu_job(file_priv, job))
 		return -EINVAL;
 
-	if (copy_from_user_with_ptr(&timestamp, ext, sizeof(timestamp)))
+	if (__c64_copy_from_user_with_ptr(drm_v3d_timestamp_query, &timestamp, ext))
 		return -EFAULT;
 
 	if (timestamp.pad)
@@ -537,7 +539,7 @@ v3d_get_cpu_reset_timestamp_params(struct drm_file *file_priv,
 	if (!v3d_validate_cpu_job(file_priv, job))
 		return -EINVAL;
 
-	if (copy_from_user_with_ptr(&reset, ext, sizeof(reset)))
+	if (__c64_copy_from_user_with_ptr(drm_v3d_reset_timestamp_query, &reset, ext))
 		return -EFAULT;
 
 	job->job_type = V3D_CPU_JOB_TYPE_RESET_TIMESTAMP_QUERY;
@@ -590,7 +592,7 @@ v3d_get_cpu_copy_query_results_params(struct drm_file *file_priv,
 	if (!v3d_validate_cpu_job(file_priv, job))
 		return -EINVAL;
 
-	if (copy_from_user_with_ptr(&copy, ext, sizeof(copy)))
+	if (__c64_copy_from_user_with_ptr(drm_v3d_copy_timestamp_query, &copy, ext))
 		return -EFAULT;
 
 	if (copy.pad)
@@ -718,7 +720,7 @@ v3d_get_cpu_reset_performance_params(struct drm_file *file_priv,
 	if (!v3d_validate_cpu_job(file_priv, job))
 		return -EINVAL;
 
-	if (copy_from_user_with_ptr(&reset, ext, sizeof(reset)))
+	if (__c64_copy_from_user_with_ptr(drm_v3d_reset_performance_query, &reset, ext))
 		return -EFAULT;
 
 	job->job_type = V3D_CPU_JOB_TYPE_RESET_PERFORMANCE_QUERY;
@@ -755,7 +757,7 @@ v3d_get_cpu_copy_performance_query_params(struct drm_file *file_priv,
 	if (!v3d_validate_cpu_job(file_priv, job))
 		return -EINVAL;
 
-	if (copy_from_user_with_ptr(&copy, ext, sizeof(copy)))
+	if (__c64_copy_from_user_with_ptr(drm_v3d_copy_performance_query, &copy, ext))
 		return -EFAULT;
 
 	if (copy.pad)
@@ -808,7 +810,7 @@ v3d_get_extensions(struct drm_file *file_priv,
 	while (user_ext) {
 		struct drm_v3d_extension ext;
 
-		if (copy_from_user_with_ptr(&ext, user_ext, sizeof(ext))) {
+		if (__c64_copy_from_user_with_ptr(drm_v3d_extension, &ext, user_ext)) {
 			drm_dbg(&v3d->drm, "Failed to copy submit extension\n");
 			return -EFAULT;
 		}
