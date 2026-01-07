19c5d38743793acba39cb056465516b44570c8f7
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-01-07 14:36:24 +0100

    cheri: UAPI: Make aio_abi.h ready for CHERI
    - The aio_context_t token must be able to contain a user pointer.
      Both userland the kernel want to dereference this.
    - The io_event structure must use capabilities for the "data" and
      "obj" fields. In order to achieve this increase the size of the
      io_event structure to four pointers by adding suitable padding.
      This ensures that a single io_event structure will not cross
      page boundaries in an event ring.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- include/uapi/linux/aio_abi.h
+++ include/uapi/linux/aio_abi.h
@@ -31,7 +31,7 @@
 #include <linux/fs.h>
 #include <asm/byteorder.h>
 
-typedef __kernel_ulong_t aio_context_t;
+typedef __ulptr aio_context_t;
 
 enum {
 	IOCB_CMD_PREAD = 0,
@@ -58,10 +58,15 @@ enum {
 
 /* read() from /dev/aio returns these structures. */
 struct io_event {
-	__u64		data;		/* the data field from the iocb */
-	__u64		obj;		/* what iocb this event came from */
-	__s64		res;		/* result code for this event */
-	__s64		res2;		/* secondary result */
+	union {
+		struct {
+			__u64ptr	data;		/* the data field from the iocb */
+			__u64ptr	obj;		/* what iocb this event came from */
+			__s64		res;		/* result code for this event */
+			__s64		res2;		/* secondary result */
+		};
+		__u64ptr	pad[4];
+	};
 };
 
 /*
