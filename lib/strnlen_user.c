30487064c9a3d47a05ebb8c96e1fbf830b579052
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-01-26 15:13:34 +0100

    cheri: lib: Revert changes to strnlen_user.c
    Revert changes to strnlen_user.c. We will fix this differently
    in the next change.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- lib/strnlen_user.c
+++ lib/strnlen_user.c
@@ -8,68 +8,6 @@
 #include <asm/word-at-a-time.h>
 
 /*
- * Returns the byte *index* of '\0' in src, or >= max if not found.
- *
- * align specifies the offset of the string (in bytes) in src; characters
- * in the range [src, src+align) are ignored.
- */
-static __always_inline long find_zero_aligned(const unsigned long __user *src,
-					      unsigned long max,
-					      unsigned long align)
-{
-	const struct word_at_a_time constants = WORD_AT_A_TIME_CONSTANTS;
-	long res = 0;
-	unsigned long c;
-
-	if (max == 0)
-		return 0;
-
-	unsafe_get_user(c, src++, efault);
-	c |= aligned_byte_mask(align);
-
-	for (;;) {
-		unsigned long data;
-		if (has_zero(c, &data, &constants)) {
-			data = prep_zero_mask(c, data, &constants);
-			data = create_zero_mask(data);
-			res += find_zero(data);
-			break;
-		}
-		res += sizeof(unsigned long);
-		/* We already handled 'unsigned long' bytes. Did we do it all ? */
-		if (unlikely(max <= sizeof(unsigned long)))
-			break;
-		max -= sizeof(unsigned long);
-		unsafe_get_user(c, src++, efault);
-	}
-
-	return res;
-efault:
-	return -EFAULT;
-}
-
-/*
- * Returns the *index* of '\0' in src, or max if not found.
- */
-static __always_inline long find_zero_unaligned(const char __user *src,
-						unsigned long max)
-{
-	long res;
-
-	for (res = 0; res < max; res++) {
-		char c;
-
-		unsafe_get_user(c, src++, efault);
-		if (c == '\0')
-			break;
-	}
-
-	return res;
-efault:
-	return -EFAULT;
-}
-
-/*
  * Do a strnlen, return length of string *with* final '\0'.
  * 'count' is the user-supplied count, while 'max' is the
  * address space maximum.
@@ -82,92 +20,52 @@ efault:
  * if it fits in a aligned 'long'. The caller needs to check
  * the return value against "> max".
  */
-static __always_inline long do_strnlen_user(const char __user *src, long count, unsigned long max)
+static __always_inline long do_strnlen_user(const char __user *src, unsigned long count, unsigned long max)
 {
-	unsigned long align, tail = 0;
-	long ret, res = 0;
-	ptraddr_t src_addr = untagged_addr(__c_pa_u(src));
-	ptraddr_t src_base = user_ptr_base(src);
-	ptraddr_t src_limit = user_ptr_limit(src);
+	const struct word_at_a_time constants = WORD_AT_A_TIME_CONSTANTS;
+	unsigned long align, res = 0;
+	unsigned long c;
 
 	/*
-	 * First check that the pointer's address is within its bounds.
-	 * If not, uaccess would fail, so return 0. Checking this now
-	 * ensures that further calculations are valid.
+	 * Do everything aligned. But that means that we
+	 * need to also expand the maximum..
 	 */
-	if (src_base > src_addr || src_limit <= src_addr)
-	    return 0;
+	align = (sizeof(unsigned long) - 1) & (unsigned long)src;
+	src -= align;
+	max += align;
 
-	align = (sizeof(unsigned long) - 1) & src_addr;
+	unsafe_get_user(c, (unsigned long __user *)src, efault);
+	c |= aligned_byte_mask(align);
 
-	if (src_limit < ALIGN(src_addr + max, sizeof(unsigned long))) {
-		/*
-		 * We cannot read all the words until src + max. Reduce max
-		 * accordingly and calculate how many tail characters will need
-		 * to be read byte by byte.
-		 */
-		max = src_limit - src_addr;
-		tail = (sizeof(unsigned long) - 1) & (src_addr + max);
-	}
-
-	if (src_base > src_addr - align || max + align == tail) {
-		/*
-		 * We cannot read the entire first aligned word, as part of it
-		 * cannot be accessed.
-		 */
-		unsigned long head;
-
-		if (max + align == tail) {
-			/*
-			 * Less than a word can be read (see limit check above)
-			 * - read everything byte by byte.
-			 */
-			head = max;
-		} else {
-			/*
-			 * Read byte by byte until the next word (or return
-			 * right away if we have already reached max).
-			 */
-			head = min(sizeof(unsigned long) - align, max);
+	for (;;) {
+		unsigned long data;
+		if (has_zero(c, &data, &constants)) {
+			data = prep_zero_mask(c, data, &constants);
+			data = create_zero_mask(data);
+			return res + find_zero(data) + 1 - align;
 		}
-
-		ret = find_zero_unaligned(src, head);
-		res += ret;
-		if (ret < head || max == head)
-			goto out;
-
-		align = 0;
-		src += head;
-		max -= head;
-	} else {
-		/* Read the entire first aligned word, adjust max accordingly. */
-		src -= align;
-		max += align;
+		res += sizeof(unsigned long);
+		/* We already handled 'unsigned long' bytes. Did we do it all ? */
+		if (unlikely(max <= sizeof(unsigned long)))
+			break;
+		max -= sizeof(unsigned long);
+		unsafe_get_user(c, (unsigned long __user *)(src+res), efault);
 	}
-
-	max -= tail;
-	ret = find_zero_aligned((unsigned long __user *)src, max, align);
-	res += ret - align;
-	if (ret < max)
-		goto out;
-
-	if (tail) {
-		ret = find_zero_unaligned(src + max, tail);
-		res += ret;
-	}
-
-out:
-	if (ret < 0)
-		return 0;
+	res -= align;
 
 	/*
-	 * find_zero_aligned() may end up reading more than count bytes.
-	 * Make sure to return the marker for "too long" in that case.
+	 * Uhhuh. We hit 'max'. But was that the user-specified maximum
+	 * too? If so, return the marker for "too long".
 	 */
 	if (res >= count)
 		return count+1;
 
-	return res+1;
+	/*
+	 * Nope: we hit the address space limit, and we still had more
+	 * characters the caller would have wanted. That's 0.
+	 */
+efault:
+	return 0;
 }
 
 /**
@@ -208,7 +106,7 @@ long strnlen_user(const char __user *str, long count)
 	}
 
 	max_addr = TASK_SIZE_MAX;
-	src_addr = (unsigned long)untagged_addr(__c_pa_u(str));
+	src_addr = (unsigned long)untagged_addr(str);
 	if (likely(src_addr < max_addr)) {
 		unsigned long max = max_addr - src_addr;
 		long retval;
