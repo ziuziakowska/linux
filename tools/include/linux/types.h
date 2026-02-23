3a5334245ee763da49351b9be40bda71d9ff41c6
Ka Leung Ho <ka.ho@codasip.com>
2026-02-23 09:48:53 +0000

    cheri: tools: add __ulptr and related types
    tools/perf/bench/futex*.c includes <linux/futex.h> which pulls in
    <linux/types.h> and expects __ulptr to be defined.
    
    Since perf prefers tools/include/linux/types.h over system headers, add
    __ulptr (matching the CHERI kernel definition) and other related types to
    allow compilation of the futex benchmarks and future-proof tools/ for
    any additional code depending on these kernel types.
    
    Signed-off-by: Ka Leung Ho <ka.ho@codasip.com>

--- tools/include/linux/types.h
+++ tools/include/linux/types.h
@@ -70,6 +70,57 @@ typedef __u64 __bitwise __be64;
 typedef __u16 __bitwise __sum16;
 typedef __u32 __bitwise __wsum;
 
+#if defined(__CHERI_PURE_CAPABILITY__)
+typedef __uintcap_t	uintptr_t;
+typedef __intcap_t	intptr_t;
+#else
+typedef unsigned long	uintptr_t;
+typedef long		intptr_t;
+#endif
+
+#ifndef __kernel_long_t
+typedef long		__kernel_long_t;
+typedef unsigned long	__kernel_ulong_t;
+#endif
+
+#ifndef __kernel_uintptr_t
+#if defined(__CHERI_PURE_CAPABILITY__)
+typedef __uintcap_t	__kernel_uintptr_t;
+typedef __intcap_t	__kernel_intptr_t;
+#else
+typedef unsigned long	__kernel_uintptr_t;
+typedef long		__kernel_intptr_t;
+#endif
+#endif
+
+#ifndef __kernel_ptraddr_t
+#ifdef __PTRADDR_TYPE__
+typedef __PTRADDR_TYPE__ __kernel_ptraddr_t;
+#else
+typedef unsigned long __kernel_ptraddr_t;
+#endif
+#endif
+
+/* An integer that can hold a pointer. */
+typedef __kernel_uintptr_t __ulptr;
+typedef __kernel_intptr_t __slptr;
+
+/* At least 64-bit and large enough for a pointer. */
+#if defined(__CHERI_PURE_CAPABILITY__)
+typedef __kernel_uintptr_t __u64ptr;
+typedef __kernel_intptr_t __s64ptr;
+#define __PACKED_IF_NOT_CHERI
+#define __CHERI_POINTER_ALIGN __attribute__((aligned(__SIZEOF_UINTCAP__)))
+#else
+typedef __u64 __u64ptr;
+typedef __s64 __s64ptr;
+#define __PACKED_IF_NOT_CHERI __attribute__((packed))
+#define __CHERI_POINTER_ALIGN
+#endif
+
+typedef __kernel_ptraddr_t __ptraddr_t;
+typedef __u64 __ptraddr64_t;
+
 #ifdef CONFIG_PHYS_ADDR_T_64BIT
 typedef u64 phys_addr_t;
 #else
@@ -94,6 +145,18 @@ typedef struct {
 
 #ifndef __aligned_le64
 # define __aligned_le64 __le64 __attribute__((aligned(8)))
+#endif
+
+/*
+ * If the CHERI types are capabilities they are naturally aligned to
+ * at least 64-bit or more.
+ */
+#if defined(__ARCH_WANT_PURECAP) || defined(__CHERI_PURE_CAPABILITY__)
+#define __aligned_u64ptr __u64ptr
+#define __aligned_s64ptr __s64ptr
+#else
+#define __aligned_u64ptr __u64ptr __attribute__((aligned(8)))
+#define __aligned_s64ptr __s64ptr __attribute__((aligned(8)))
 #endif
 
 struct list_head {
