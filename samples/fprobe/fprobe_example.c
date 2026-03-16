be6669d7af6256ee4bd41ac0d9eff80712de013c
Martin Kaiser <martin.kaiser@codasip.com>
2026-03-16 16:30:01 +0100

    fprobe_example: update the module for cheri
    Update the fprobe_example demo module for cheri.
    
    The entry and exit handlers get a full capability for ret_ip.
    
    Addresses are converted to fake pointers for printing.
    
    Signed-off-by: Martin Kaiser <martin.kaiser@codasip.com>

--- samples/fprobe/fprobe_example.c
+++ samples/fprobe/fprobe_example.c
@@ -49,7 +49,7 @@ static void show_backtrace(void)
 }
 
 static int sample_entry_handler(struct fprobe *fp, unsigned long ip,
-				unsigned long ret_ip,
+				uintptr_t ret_ip,
 				struct ftrace_regs *fregs, void *data)
 {
 	if (use_trace)
@@ -57,9 +57,9 @@ static int sample_entry_handler(struct fprobe *fp, unsigned long ip,
 		 * This is just an example, no kernel code should call
 		 * trace_printk() except when actively debugging.
 		 */
-		trace_printk("Enter <%pS> ip = 0x%p\n", (void *)ip, (void *)ip);
+		trace_printk("Enter <%pS> ip = 0x%p\n", __c_fakep(ip), __c_fakep(ip));
 	else
-		pr_info("Enter <%pS> ip = 0x%p\n", (void *)ip, (void *)ip);
+		pr_info("Enter <%pS> ip = 0x%p\n", __c_fakep(ip), __c_fakep(ip));
 	nhit++;
 	if (stackdump)
 		show_backtrace();
@@ -67,10 +67,10 @@ static int sample_entry_handler(struct fprobe *fp, unsigned long ip,
 }
 
 static void sample_exit_handler(struct fprobe *fp, unsigned long ip,
-				unsigned long ret_ip, struct ftrace_regs *regs,
+				uintptr_t ret_ip, struct ftrace_regs *regs,
 				void *data)
 {
-	unsigned long rip = ret_ip;
+	unsigned long rip = __c_ua(ret_ip);
 
 	if (use_trace)
 		/*
@@ -78,10 +78,10 @@ static void sample_exit_handler(struct fprobe *fp, unsigned long ip,
 		 * trace_printk() except when actively debugging.
 		 */
 		trace_printk("Return from <%pS> ip = 0x%p to rip = 0x%p (%pS)\n",
-			(void *)ip, (void *)ip, (void *)rip, (void *)rip);
+			__c_fakep(ip), __c_fakep(ip), __c_fakep(rip), __c_fakep(rip));
 	else
 		pr_info("Return from <%pS> ip = 0x%p to rip = 0x%p (%pS)\n",
-			(void *)ip, (void *)ip, (void *)rip, (void *)rip);
+			__c_fakep(ip), __c_fakep(ip), __c_fakep(rip), __c_fakep(rip));
 	nhit++;
 	if (stackdump)
 		show_backtrace();
