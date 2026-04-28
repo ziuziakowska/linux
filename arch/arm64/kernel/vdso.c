29fa054e0aaa66f6122f377916b2119d9a790c63
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:55:14 +0200

    [CHERRY-PICK] arm64: vdso: Create appropriate reservation
    Cherry pick "6f4ab0b3b4ec arm64: vdso: Create appropriate reservation":
    | commit 6f4ab0b3b4ec9137412ee5a18b0a69eaaca3233d
    | Author: Kevin Brodsky <kevin.brodsky@arm.com>
    | Date:   Tue Apr 23 20:26:22 2024 +0100
    |
    |     arm64: vdso: Create appropriate reservation
    |
    |     Create an appropriate reservation for the vDSO spanning both
    |     mappings.
    |
    |     Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -20,6 +20,7 @@
 #include <linux/slab.h>
 #include <linux/vdso_datastore.h>
 #include <linux/vmalloc.h>
+#include <linux/mman.h>
 #include <vdso/datapage.h>
 #include <vdso/helpers.h>
 #include <vdso/vsyscall.h>
@@ -101,9 +102,9 @@ static int __setup_additional_pages(enum vdso_abi abi,
 				    struct linux_binprm *bprm,
 				    int uses_interp)
 {
-	unsigned long vdso_base, vdso_text_len, vdso_mapping_len;
+	unsigned long vdso_base, vdso_text_base, vdso_text_len, vdso_mapping_len;
 	unsigned long gp_flags = 0;
-	void *ret;
+	struct vm_area_struct *ret;
 
 	BUILD_BUG_ON(VDSO_NR_PAGES != __VDSO_PAGES);
 
@@ -121,17 +122,25 @@ static int __setup_additional_pages(enum vdso_abi abi,
 	if (IS_ERR(ret))
 		goto up_fail;
 
+	if (reserv_vma_set_reserv(ret, vdso_base, vdso_mapping_len,
+				  PROT_READ | PROT_EXEC))
+		goto up_fail;
+
 	if (system_supports_bti_kernel())
 		gp_flags = VM_ARM64_BTI;
 
-	vdso_base += VDSO_NR_PAGES * PAGE_SIZE;
-	mm->context.vdso = (void *)vdso_base;
-	ret = _install_special_mapping(mm, vdso_base, vdso_text_len,
+	vdso_text_base = vdso_base + VDSO_NR_PAGES * PAGE_SIZE;
+	mm->context.vdso = (void *)vdso_text_base;
+	ret = _install_special_mapping(mm, vdso_text_base, vdso_text_len,
 				       VM_READ|VM_EXEC|gp_flags|
 				       VM_MAYREAD|VM_MAYWRITE|VM_MAYEXEC|
 				       VM_SEALED_SYSMAP,
 				       vdso_info[abi].cm);
 	if (IS_ERR(ret))
+		goto up_fail;
+
+	if (reserv_vma_set_reserv(ret, vdso_base, vdso_mapping_len,
+				  PROT_READ | PROT_EXEC))
 		goto up_fail;
 
 	return 0;
