f6e706a54464c4d6136078165755a82d5081ae28
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:49:58 +0200

    [CHERRY-PICK] linux/{align.h,mm.h}: Introduce user pointer align helpers
    Cherry pick "7ca9c85a6375 linux/{align.h,mm.h}: Introduce user pointer align helpers":
    | commit 7ca9c85a6375b1cc8ea403288e2ba5841a424473
    | Author: Kevin Brodsky <kevin.brodsky@arm.com>
    | Date:   Thu Jun 2 11:56:15 2022 +0100
    |
    |     linux/{align.h,mm.h}: Introduce user pointer align helpers
    |
    |     There is currently no alignment helper able to handle user pointers,
    |     especially when they are capabilities like in the pure-capability
    |     uABI. Bridge this gap by adding new USER_PTR_*() helpers.
    |
    |     Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- include/vdso/align.h
+++ include/vdso/align.h
@@ -10,6 +10,32 @@
 #define __ALIGN_MASK(x, mask)	__ALIGN_KERNEL_MASK((x), (mask))
 #define PTR_ALIGN(p, a)		((typeof(p))ALIGN((uintptr_t)(p), (a)))
 #define PTR_ALIGN_DOWN(p, a)	((typeof(p))ALIGN_DOWN((uintptr_t)(p), (a)))
+
+#ifdef CONFIG_CHERI_PURECAP_UABI
+#ifdef CONFIG_CHERI_KERNEL
+#define USER_PTR_ALIGN(p, a)		((typeof(p))ALIGN((user_uintptr_t)(p), (a)))
+#define USER_PTR_ALIGN_DOWN(p, a)	((typeof(p))ALIGN_DOWN((user_uintptr_t)(p), (a)))
+#else
+/* TODO [PCuABI] - use __builtin_align_{down,up} once GCC supports them */
+#define USER_PTR_ALIGN(p, a)						\
+({									\
+	typeof(p) __p = (p);						\
+	(typeof(__p))__builtin_cheri_address_set(__p,			\
+		ALIGN(user_ptr_addr(__p), ((unsigned long)(a)))); 	\
+})
+#define USER_PTR_ALIGN_DOWN(p, a)					\
+({									\
+	typeof(p) __p = (p);						\
+	(typeof(__p))__builtin_cheri_address_set(__p,			\
+		ALIGN_DOWN(user_ptr_addr(__p), ((unsigned long)(a))));	\
+})
+#endif
+#else /* CONFIG_CHERI_PURECAP_UABI */
+#define USER_PTR_ALIGN		PTR_ALIGN
+#define USER_PTR_ALIGN_DOWN	PTR_ALIGN_DOWN
+#endif /* CONFIG_CHERI_PURECAP_UABI */
+#define USER_PTR_PAGE_ALIGN(ptr) USER_PTR_ALIGN((ptr), PAGE_SIZE)
+
 #define IS_ALIGNED(x, a)		(((x) & ((typeof(x))(a) - 1)) == 0)
 
 #endif	/* __VDSO_ALIGN_H */
