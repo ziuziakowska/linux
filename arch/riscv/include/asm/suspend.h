4d6ccb082489a6c6c1eb29893146dbdf285bec5b
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:36:23 +0200

    cheri: riscv: suspend: Make the saved trap vector a capability
    The saved trap vector for suspend must be a capability.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- arch/riscv/include/asm/suspend.h
+++ arch/riscv/include/asm/suspend.h
@@ -14,7 +14,7 @@ struct suspend_context {
 	struct pt_regs regs;
 	/* Saved and restored by high-level functions */
 	unsigned long envcfg;
-	unsigned long tvec;
+	uintptr_t tvec;
 	unsigned long ie;
 #ifdef CONFIG_MMU
 	unsigned long satp;
