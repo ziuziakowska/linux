5d4a07c07132dfd9cdae72b3b1fb9c6cf10d60d2
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:49:56 +0200

    [CHERRY-PICK] io_uring/rw: Restrict copy to only uiov->len from userspace
    Cherry pick "ce1bee7a6797 io_uring/rw: Restrict copy to only uiov->len from userspace":
    | commit ce1bee7a67975d32602ac1b8f22bbb37b7950aa9
    | Author: Tudor Cretu <tudor.cretu@arm.com>
    | Date:   Fri Jan 6 13:58:56 2023 +0000
    |
    |     io_uring/rw: Restrict copy to only uiov->len from userspace
    |
    |     Only the len member is needed, so restrict the copy_from_user to that.
    |
    |     Signed-off-by: Tudor Cretu <tudor.cretu@arm.com>
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- io_uring/rw.c
+++ io_uring/rw.c
@@ -64,7 +64,6 @@ static int io_iov_compat_buffer_select_prep(struct io_rw *rw)
 static int io_iov_buffer_select_prep(struct io_kiocb *req)
 {
 	struct iovec __user *uiov;
-	struct iovec iov;
 	struct io_rw *rw = io_kiocb_to_cmd(req, struct io_rw);
 
 	if (rw->len != 1)
@@ -74,9 +73,8 @@ static int io_iov_buffer_select_prep(struct io_kiocb *req)
 		return io_iov_compat_buffer_select_prep(rw);
 
 	uiov = u64_to_user_ptr(rw->addr);
-	if (copy_from_user_with_ptr(&iov, uiov, sizeof(*uiov)))
+	if (get_user(rw->len, &uiov->iov_len))
 		return -EFAULT;
-	rw->len = iov.iov_len;
 	return 0;
 }
 
