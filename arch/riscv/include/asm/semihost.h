5349a371c66c7c0b611a5f37f5a19c87e5f9b49d
Christian A. Ehrhardt <christian.ehrhardt@codasip.com>
2026-04-28 15:35:40 +0200

    cheri: riscv: Fix inline assembly in asm/semihost.h
    Use capability register and constraints.
    
    Signed-off-by: Christian A. Ehrhardt <christian.ehrhardt@codasip.com>

--- arch/riscv/include/asm/semihost.h
+++ arch/riscv/include/asm/semihost.h
@@ -7,11 +7,13 @@
 #ifndef _RISCV_SEMIHOST_H_
 #define _RISCV_SEMIHOST_H_
 
+#include <asm/asm.h>
+
 struct uart_port;
 
 static inline void smh_putc(struct uart_port *port, unsigned char c)
 {
-	asm volatile("addi    a1, %0, 0\n"
+	asm volatile(CINSN(addi)" "CREG(a1)", %0, 0\n"
 		     "addi    a0, zero, 3\n"
 		     ".balign 16\n"
 		     ".option push\n"
@@ -20,7 +22,7 @@ static inline void smh_putc(struct uart_port *port, unsigned char c)
 		     "ebreak\n"
 		     "srai    zero, zero, 0x7\n"
 		     ".option pop\n"
-		     : : "r" (&c) : "a0", "a1", "memory");
+		     : : PTRC (&c) : "a0", "a1", "memory");
 }
 
 #endif /* _RISCV_SEMIHOST_H_ */
